190b781edcaf3c79f86c6d22d24ab51445894799
RandomByte <r@ndombyte.net>
2016-07-17 15:54:29 +0200

    Implemented mqtt client publish

--- index.js
+++ index.js
@@ -1,24 +1,67 @@
 var gpio = require('omega_gpio'),
 	Button = gpio.Button,
-	sensor;
+	mqtt = require('mqtt'),
+	oConfig = require('./config.json'),
 
-sensor = new Button(1);
+	iResetMotionTimeout, sTopic,
+	oSensor, oClient;
+
+/* Check config */
+if (!oConfig.site || !oConfig.room || !oConfig.brokerUrl) {
+	onsole.log("There's something missing in your config.json, please refer to config.example.json for an example");
+    process.exit(1);
+}
+
+sTopic = oConfig.site + "/" + oConfig.room + "/Motion";
+oClient = mqtt.connect(oConfig.brokerUrl);
+
+oSensor = new Button(1);
 
 // Clean up the pins on exit
 process.on('SIGINT', function () {
 	console.log("Cleaning up...");
-	sensor.destroy();
+	oSensor.destroy(); // If this doesn't help: 'echo 1 > /sys/class/gpio/gpiochip0/subsystem/unexport'
 	process.exit();
 });
 
-
-var checkSensor = function(){
-  if(sensor.isPressed()){
-    console.log("motion" + new Date().getTime());
-  }
-  else{
-    // console.log("no motion");
-  }
+function checkSensor (){
+	if(oSensor.isPressed()){
+		console.log("motion" + new Date().getTime());
+		motion();
+	}
 }
 
+function motion () {
+	if (!iResetMotionTimeout) { // Last state was: no motion
+		// New state: motion
+		// -> publish state change to broker
+		oClient.publish(sTopic, "true", {
+			qos: 2, // must arrive and must arrive exactly once - also ensures order
+		});
+	} else {
+		// Last state was already motion
+		// -> reset timeout
+		clearTimeout(iResetMotionTimeout)
+
+	}
+	iResetMotionTimeout = setTimeout(resetMotion, 5000);	
+}
+
+function resetMotion () {
+	iResetMotionTimeout = null;
+	// New state: no motion
+	// -> publish state change to broker
+	oClient.publish(sTopic, "false", {
+		qos: 2, // must arrive and must arrive exactly once - also ensures order
+	});
+}
+
+
 setInterval(checkSensor, 10);
+
+
+
+
+
+
+
